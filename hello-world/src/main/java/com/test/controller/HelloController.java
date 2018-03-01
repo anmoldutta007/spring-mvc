192e0e2251a4de7af72a17d08db622f46e32f7c3
Anmol Dutta <anmoldutta007@gmail.com>
2018-03-01 22:19:30 +0530

    Hello World spring mvc

---
+++ hello-world/src/main/java/com/test/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping("/hello")
+	public ModelAndView hello(){
+		ModelAndView modelAndView = new ModelAndView("hello");
+		modelAndView.addObject("msg", "Hello User");
+		return modelAndView;
+	}
+	
+	@RequestMapping("/hi")
+	public ModelAndView hi(){
+		ModelAndView modelAndView = new ModelAndView("hello");
+		modelAndView.addObject("msg", "Hi User");
+		return modelAndView;
+	}
+}
